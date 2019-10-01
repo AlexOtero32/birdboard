ab9cda22a4d292142e2e2c1c76783a3f5526d850
alejandro <alejandro@kuzunguka.com>
2019-10-01 19:00:38 +0200

    Un proyecto pertenece a un usuario

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -13,6 +13,16 @@ class ProjectsTest extends TestCase
 
     /**
      * @test
+     */
+    public function only_authenticated_users_can_create_projects()
+    {
+        $attributes = factory('App\Project')->raw(['owner_id' => null]);
+
+        $this->post('/projects', $attributes)->assertRedirect('/login');
+    }
+
+    /**
+     * @test
      *
      * @return void
      */
@@ -20,6 +30,8 @@ class ProjectsTest extends TestCase
     {
 
         $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = [
             'title' => $this->faker->sentence,
@@ -39,6 +51,8 @@ class ProjectsTest extends TestCase
 
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -49,6 +63,8 @@ class ProjectsTest extends TestCase
      */
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
