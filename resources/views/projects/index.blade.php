4bc850f71aae3caf40b02aff27dea30535927334
alejandro <alejandro@kuzunguka.com>
2019-10-03 17:38:09 +0200

    Modificado CSS de página de proyecto

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex items-center">
-    <h1 class="mr-auto">Proyectos</h1>
-    <a href="/projects/create">Nuevo proyecto</a>
-</div>
+<header class="flex items-center mb-3 py-4">
+    <div class="flex justify-between w-full items-center">
+        <h1 class="text-gray-600 text-sm font-normal">Mis proyectos</h1>
+        <a href="/projects/create" class="button">Nuevo proyecto</a>
+    </div>
+</header>
 
-<div class="flex">
+<div class="md:flex md:flex-wrap -mx-3">
     @forelse ($projects as $project)
-    <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 250px;">
-        <h3 class="font-normal text-xl py-4">{{$project->title}}</h3>
-        <div class="text-gray-700">{{ Illuminate\Support\Str::limit($project->description) }}</div>
+    <div class="md:w-1/3 px-2 pb-6">
+        @include('projects.card')
     </div>
     @empty
     <div>No hay proyectos</div>
--- tailwind.js
+++ tailwind.js
@@ -1,12 +1,13 @@
 module.exports = {
     theme: {
-        extend: {},
-        boxShadow: {
-            default: "0 0 5px 0 rgba(0, 0, 0, .08)"
-        },
-        colors: {
-            grey: {
-                "light-gray": "#F5F6F9"
+        extend: {
+            colors: {
+                grey: {
+                    "light-gray": "#F5F6F9"
+                }
+            },
+            boxShadow: {
+                default: "0 0 5px 0 rgba(0, 0, 0, .08)"
             }
         }
     },
